2a102ce078c8eb1ebbb5123b0913d8fabd8148d7
OxY623 <fuzzydoorproductions0@gmail.com>
2024-08-31 07:42:39 +0000

    Fixing

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import './index.css'
+import { DevSupport } from '@react-buddy/ide-toolbox'
+
 import store from './store'
 import App from './components/App'
+import { ComponentPreviews, useInitial } from './dev'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <DevSupport
+        ComponentPreviews={ComponentPreviews}
+        useInitialHook={useInitial}
+      >
+        <App />
+      </DevSupport>
     </Provider>
   </React.StrictMode>,
 )
